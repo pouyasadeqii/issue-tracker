84b950223ee9b9b7002912a93e0a973532827027
Pouya Sadeghimirani <pooya@arianalabs.io>
2026-06-06 19:40:47 +0330

    feat: add loading component to the app

---
+++ app/loading.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Loading = () => {
+    return (
+        <span className="loading loading-spinner loading-md">Loading...</span>
+    );
+};
+
+export default Loading;

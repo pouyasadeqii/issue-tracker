26242db26ae04adc3265576bed5e0b8390f12c69
Pouya Sadeghimirani <pooya@arianalabs.io>
2026-06-06 18:12:24 +0330

    feat: add nevbar and define another layout for Admin route & overide h1 element with tailwind @layer

---
+++ app/Navbar.tsx
@@ -0,0 +1,14 @@
+import Link from 'next/link';
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <div className="flex bg-slate-200 p-5">
+            <Link href="/" className='mr-5'>Home</Link>
+            <Link href="/users" className='mr-5'>Users</Link>
+            <Link href="/admin" className='mr-5'>Admin</Link>
+        </div>
+    );
+};
+
+export default Navbar;
